e587a0a94158c375a4fa59f01863571662a959f9
Colby <30965751+col-bc@users.noreply.github.com>
2022-05-16 10:01:31 -0400

    Remove webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        'crypto': require.resolve('crypto-browserify'),
-        'stream': require.resolve('stream-browserify'),
-        'util': require.resolve('util'),
-      },
-    },
-  }
+  // configureWebpack: {
+  //   resolve: {
+  //     fallback: {
+  //       'crypto': require.resolve('crypto-browserify'),
+  //       'stream': require.resolve('stream-browserify'),
+  //       'util': require.resolve('util'),
+  //     },
+  //   },
+  // }
 })
